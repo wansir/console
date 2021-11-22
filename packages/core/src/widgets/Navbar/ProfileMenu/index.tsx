df58ecaa16d346b08cbabeaf85117f93d774282e
chenz24 <chenz8606@gmail.com>
2021-11-22 15:59:52 +0800

    refactor: Move Navbar to widgets.

--- packages/core/src/widgets/Navbar/ProfileMenu/index.tsx
+++ packages/core/src/widgets/Navbar/ProfileMenu/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Human, CaretDown, Wrench, Logout, Information } from '@kubed/icons';
-import { Dropdown, Menu, MenuItem } from '@kubed/components';
+import { Dropdown, Menu, MenuItem, useModal } from '@kubed/components';
 
 import { LoginWrapper, NotLogin } from './styles';
+import About from '../About';
 
 interface ProfileMenuProps {
   isLogin: boolean;
@@ -10,6 +11,8 @@ interface ProfileMenuProps {
 }
 
 const ProfileMenu = ({ isLogin, isAppsPage }: ProfileMenuProps) => {
+  const modal = useModal();
+
   if (!isLogin) {
     return (
       <NotLogin>
@@ -19,11 +22,24 @@ const ProfileMenu = ({ isLogin, isAppsPage }: ProfileMenuProps) => {
     );
   }
 
+  const openAboutModal = () => {
+    modal.open({
+      title: 'about',
+      header: <div />,
+      closable: false,
+      // @ts-ignore
+      footer: <div />,
+      content: <About />,
+    });
+  };
+
   const userMenu = (
     <Menu>
       <MenuItem icon={<Wrench />}>{t('USER_SETTINGS')}</MenuItem>
       <MenuItem icon={<Logout />}>{t('LOG_OUT')}</MenuItem>
-      <MenuItem icon={<Information />}>{t('ABOUT')}</MenuItem>
+      <MenuItem icon={<Information />} onClick={openAboutModal}>
+        {t('ABOUT')}
+      </MenuItem>
     </Menu>
   );
 
