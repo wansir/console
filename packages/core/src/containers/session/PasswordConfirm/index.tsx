b8bf3e2da6826d07bf919ae2733cb31857464afa
chenz24 <chenz8606@gmail.com>
2021-12-02 18:34:16 +0800

    feat: Add PasswordTip component.

--- packages/core/src/containers/session/PasswordConfirm/index.tsx
+++ packages/core/src/containers/session/PasswordConfirm/index.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { get } from 'lodash';
-import { Success } from '@kubed/icons';
 import { Button, Card, Form, FormItem, InputPassword, Alert } from '@kubed/components';
 import { request, Pattern } from '@ks-console/shared';
+import PasswordTip from '../../../components/PasswordTip';
 
 import {
   LoginHeader,
@@ -13,7 +13,6 @@ import {
   LoginDivider,
   LoginButton,
 } from '../Login/styles';
-import { PasswordTipWrapper, TipInner, TipItem } from './styles';
 
 const LoginConfirm = () => {
   const [password, setPassword] = useState('');
@@ -100,29 +99,7 @@ const LoginConfirm = () => {
           >
             <InputPassword name="password" />
           </FormItem>
-          <PasswordTipWrapper>
-            <p>{t('Your password must meet the following requirements')}</p>
-            <TipInner>
-              <TipItem>
-                <Success
-                  size={16}
-                  variant={Pattern.PATTERN_WORD.test(password) ? 'coloured' : 'dark'}
-                />
-                {t('At least 1 uppercase and lowercase letter')}
-              </TipItem>
-              <TipItem>
-                <Success
-                  size={16}
-                  variant={Pattern.PATTERN_NUMBER.test(password) ? 'coloured' : 'dark'}
-                />
-                {t('At least 1 number')}
-              </TipItem>
-              <TipItem>
-                <Success size={16} variant={password.length >= 6 ? 'coloured' : 'dark'} />
-                {t('Password length is at least 6 characters')}
-              </TipItem>
-            </TipInner>
-          </PasswordTipWrapper>
+          <PasswordTip password={password} className="mt12" />
           <LoginButton>
             <Button
               color="secondary"
