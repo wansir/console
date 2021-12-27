fa3b1e002e8dd1303272a7b0b68bda418673e034
chenz24 <chenz8606@gmail.com>
2021-12-27 14:36:23 +0800

    feat: Add cluster index page.

--- packages/clusters/src/containers/Clusters/index.tsx
+++ packages/clusters/src/containers/Clusters/index.tsx
@@ -1,7 +1,171 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Cluster, Refresh } from '@kubed/icons';
+import {
+  Banner,
+  Button,
+  FilterInput,
+  Card,
+  Empty,
+  Text,
+  Badge,
+  Group,
+  Loading,
+} from '@kubed/components';
+import { getActions } from '@ks-console/shared';
+import { isEmpty } from 'lodash';
+import { fetchList } from '../../stores/cluster';
+import ClusterCard from '../../components/ClusterCard';
+
+import { ClustersWrapper, Toolbar, Main, ClusterList } from './styles';
 
 const Clusters = () => {
-  return <div>Cluster</div>;
+  const [name, setName] = useState('');
+  const {
+    loading: hostLoading,
+    data: hostData = [],
+    refresh: hostRefresh,
+    reFetch: hostReFetch,
+  } = fetchList({
+    labelSelector: 'cluster-role.kubesphere.io/host=',
+    limit: -1,
+  });
+
+  const {
+    loading,
+    data = [],
+    refresh,
+    total,
+    reFetch,
+  } = fetchList({
+    labelSelector: '!cluster-role.kubesphere.io/host',
+  });
+
+  const enableActions = React.useMemo(() => {
+    return getActions({ module: 'clusters' });
+  }, []);
+
+  const handleRefresh = () => {
+    hostRefresh();
+    refresh();
+  };
+
+  const handleSearch = (keyword: string) => {
+    console.log('keyword', keyword);
+    setName(keyword);
+    reFetch({ name: keyword });
+    hostReFetch({ name: keyword });
+  };
+
+  const renderToolbar = () => {
+    return (
+      <Toolbar>
+        <FilterInput
+          className="mr12 search-bar"
+          placeholder={t('SEARCH_BY_NAME')}
+          onChange={handleSearch}
+          simpleMode
+        />
+        <Button variant="text" onClick={handleRefresh}>
+          <Refresh />
+        </Button>
+        {enableActions.includes('create') ? (
+          <Button color="secondary" shadow className="ml12 btn-add">
+            {t('ADD_CLUSTER')}
+          </Button>
+        ) : null}
+      </Toolbar>
+    );
+  };
+
+  const noResultTitle = (
+    <Group spacing="sm">
+      <Text weight={600} size={14}>
+        {t('Cluster List')}
+      </Text>
+      <Badge color="#abb4be">{total}</Badge>
+    </Group>
+  );
+
+  const renderHost = () => (
+    <ClusterList className="mb12">
+      <div className="cluster-title">
+        {hostData.length === 1 ? t('HOST_CLUSTER_TCAP') : t('HOST_CLUSTER_PL_TCAP')}
+      </div>
+      {hostData.map((item: any) => (
+        <ClusterCard key={item.name} data={item} />
+      ))}
+    </ClusterList>
+  );
+
+  const renderClusters = () => (
+    <ClusterList>
+      <Group spacing="sm" className="mb8">
+        <Text weight={600} size={14}>
+          {t('Member Clusters')}
+        </Text>
+        <Badge color="#abb4be">{total}</Badge>
+      </Group>
+      {data.map((item: any) => (
+        <ClusterCard key={item.name} data={item} className="mb12" />
+      ))}
+    </ClusterList>
+  );
+
+  const renderList = () => {
+    if (loading || hostLoading) {
+      return <Loading className="page-loading" />;
+    }
+
+    if (!loading && !hostLoading && !name && isEmpty(data) && isEmpty(hostData)) {
+      return (
+        <Card padding={32}>
+          <Empty
+            title={t('NO_CLUSTER_TIP')}
+            description={t('NO_CLUSTER_TIP_DESC')}
+            image={<Cluster size={48} />}
+            imageClassName="empty-icon"
+          >
+            <Button color="secondary" shadow className="btn-add" style={{ marginTop: '20px' }}>
+              {t('ADD_CLUSTER')}
+            </Button>
+          </Empty>
+        </Card>
+      );
+    }
+
+    if (!loading && !hostLoading && isEmpty(data) && isEmpty(hostData)) {
+      return (
+        <Card className="mt12" padding={32} sectionTitle={noResultTitle}>
+          <Empty
+            title={t('NO_RESOURCE_FOUND')}
+            image={<img src="/assets/empty-card.svg" />}
+            imageStyle={{ width: '100%', height: '100%', background: 'none' }}
+          />
+        </Card>
+      );
+    }
+
+    return (
+      <>
+        {renderHost()}
+        {renderClusters()}
+      </>
+    );
+  };
+
+  return (
+    <ClustersWrapper>
+      <Banner
+        className="cluster-banner"
+        icon={<Cluster />}
+        title={t('Clusters Management')}
+        description={t('CLUSTERS_MANAGE_DESC')}
+      >
+        {renderToolbar()}
+      </Banner>
+      <Main>{renderList()}</Main>
+    </ClustersWrapper>
+  );
 };
 
 export default Clusters;
--- packages/shared/src/constants/common.ts
+++ packages/shared/src/constants/common.ts
@@ -1,6 +1,97 @@
-export const API_VERSIONS = {
+export const API_VERSIONS: Record<string, string> = {
   users: 'apis/iam.kubesphere.io/v1alpha2',
   globalroles: 'apis/iam.kubesphere.io/v1alpha2',
+};
+
+export const COMPONENT_ICON_MAP: Record<string, string> = {
+  kubernetes: 'kubernetes',
+  kubesphere: 'kubesphere',
+  istio: 'istio',
+  openpitrix: 'openpitrix',
+  devops: 'jenkins',
+  logging: 'record',
+  monitoring: 'monitor',
+  alerting: 'loudspeaker',
+  auditing: 'login-servers',
+  events: 'thunder',
+  notification: 'mail',
+  servicemesh: 'istio',
+  metrics_server: 'monitor',
+};
+
+export const CLUSTER_PROVIDER_ICON: Record<string, string> = {
+  'Aliyun ACK': 'aliyun',
+  'Azure Kubernetes Service': 'windows',
+  'Huawei Cloud CCE': 'kubernetes',
+  'Amazon EKS': 'aws',
+  'Google Kubernetes Engine': 'google-plus',
+  'QingCloud Kubernetes Engine': 'qingcloud',
+  'Tencent Kubernetes Engine': 'kubernetes',
+  kubesphere: 'kubernetes',
+};
+
+export const CLUSTER_PROVIDERS = [
+  {
+    label: 'Alibaba Cloud ACK',
+    value: 'Aliyun ACK',
+    icon: 'aliyun',
+  },
+  {
+    label: 'Azure Kubernetes Service',
+    value: 'Aure Kubernetes Service',
+    icon: 'windows',
+  },
+  {
+    label: 'Huawei Cloud CCE',
+    value: 'Huawei Cloud CCE',
+    icon: 'kubernetes',
+  },
+  {
+    label: 'Amazon EKS',
+    value: 'Amazon EKS',
+    icon: 'aws',
+  },
+  {
+    label: 'Google Kubernetes Engine',
+    value: 'Google Kubernetes Engine',
+    icon: 'google-plus',
+  },
+  {
+    label: 'QingCloud Kubernetes Engine',
+    value: 'QingCloud Kubernetes Engine',
+    icon: 'qingcloud',
+  },
+  {
+    label: 'Tencent Kubernetes Engine',
+    value: 'Tencent Kubernetes Engine',
+    icon: 'kubernetes',
+  },
+];
+
+export const CLUSTER_PRESET_GROUPS = [
+  {
+    label: 'production',
+    value: 'production',
+  },
+  {
+    label: 'development',
+    value: 'development',
+  },
+  {
+    label: 'testing',
+    value: 'testing',
+  },
+  {
+    label: 'demo',
+    value: 'demo',
+  },
+];
+
+export const CLUSTER_GROUP_TAG_TYPE: Record<string, string> = {
+  production: 'warning',
+  development: 'default',
+  testing: 'info',
+  demo: 'primary',
 };
 
 export const DEFAULT_CLUSTER = {
