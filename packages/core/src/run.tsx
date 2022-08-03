54dcd747c8d1dcf354743afbfdf5ee20159b8ddc
chenz24 <chenz8606@gmail.com>
2022-08-03 22:02:00 +0800

    refactor: clean codes.
    Signed-off-by: chenz24 <chenz8606@gmail.com>

--- packages/core/src/run.tsx
+++ packages/core/src/run.tsx
@@ -2,15 +2,11 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Loading } from '@kubed/components';
 import App from './App';
-// import plugins from './plugins';
 import loader from './libs/loader';
 import i18n from './libs/i18n';
 import emitter from './libs/emitter';
 
 const run = async () => {
-  // load local plugin
-  // globals.context.registerPlugins(plugins);
-
   if (process.env.NODE_ENV === 'production') {
     // load remote plugin
     await loader(globals.installedPlugins);
