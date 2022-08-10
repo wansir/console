526fa6c1115b207db843f7e73b057c2f1db0e590
donniean <acmilank22@gmail.com>
2022-08-10 17:51:29 +0800

    feat: add Banner and DataTable in Accounts

--- packages/access/src/containers/Accounts/index.tsx
+++ packages/access/src/containers/Accounts/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
+import { Banner } from '@kubed/components';
+import { Human } from '@kubed/icons';
+import { DataTable } from '@ks-console/shared';
+import type { Column } from '@ks-console/shared';
+
 export default function Accounts() {
-  return <h1>Accounts</h1>;
+  const columns: Column[] = [
+    {
+      title: t('NAME'),
+      field: 'spec.names.kind',
+    },
+    {
+      title: t('STATUS'),
+      field: 'metadata.name',
+    },
+    {
+      title: t('PLATFORM_ROLE'),
+      field: 'spec.scope',
+    },
+    {
+      title: t('LAST_LOGIN'),
+      field: 'metadata.creationTimestamp',
+    },
+  ];
+
+  return (
+    <>
+      <Banner icon={<Human />} title={t('USER_PL')} description={t('USER_DESC')} className="mb12" />
+      <DataTable columns={columns} tableName="users" rowKey="name" />
+    </>
+  );
 }
