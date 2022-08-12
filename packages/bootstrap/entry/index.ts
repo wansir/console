a4ac60fed2245496e321f8bf0862bf5fe34e0a75
Bunny <qimiao@yunify.com>
2022-08-12 16:01:05 +0800

    refactor: Change the version of platfrom settings plugin
    Signed-off-by: Bunny <qimiao@yunify.com>

--- packages/bootstrap/entry/index.ts
+++ packages/bootstrap/entry/index.ts
@@ -1,11 +1,11 @@
 import { devtools } from '@kubed/stook-devtools';
 import { run, Context } from '@ks-console/core';
-import plugins from './plugins';
+import extensions from './extensions';
 
 globals.context = new Context();
 
-// load local plugin
-globals.context.registerPlugins(plugins);
+// load local extension
+globals.context.registerExtensions(extensions);
 
 if (process.env.NODE_ENV === 'production') {
   run();
