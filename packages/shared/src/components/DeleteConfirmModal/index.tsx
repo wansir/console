d05722254603596c22883071fbb3168cbe17fc82
donniean <acmilank22@gmail.com>
2022-08-19 12:09:24 +0800

    feat: dev DeleteConfirmModal

--- packages/shared/src/components/DeleteConfirmModal/index.tsx
+++ packages/shared/src/components/DeleteConfirmModal/index.tsx
@@ -1,35 +1,61 @@
 import React from 'react';
-// import type { ReactNode } from 'react';
-import { Modal, Input, FormItem } from '@kubed/components';
-// import type { ModalProps } from '@kubed/components';
+import type { ReactNode } from 'react';
+import { Modal, Form, FormItem, Input, Button } from '@kubed/components';
+import type { ModalProps } from '@kubed/components';
+import { Close } from '@kubed/icons';
 
-import { StyledForm, Header, Title, StyledCloseIcon } from './styles';
+import {
+  Body,
+  Header,
+  Title,
+  CloseIconWrapper,
+  Content,
+  Tip,
+  Footer,
+  StyledButton,
+} from './styles';
 
-interface DeleteModalProps {
-  type: string;
-  resource: string;
+interface DeleteModalProps extends Omit<ModalProps, 'title'> {
+  type?: string;
+  resource?: string | number | string[] | number[];
   deleteCluster?: boolean;
   app?: any;
-  // title: ReactNode;
-  // tips: ReactNode;
-  // resource: string | number | string[] | number[];
+  title?: ReactNode;
+  tip?: ReactNode;
 }
 
 export type { DeleteModalProps };
 
 export default function DeleteConfirmModal({
   type,
-  resource,
+  resource: resourceProp,
   deleteCluster,
   app,
+  title: titleProp,
+  tip: tipProp,
+  confirmLoading,
+  ...rest
 }: DeleteModalProps) {
   const typeKey = type || undefined;
   const typeKeyLow = type ? `${type}_LOW` : undefined;
   const typeKeyPl = type ? `${type}_PL` : undefined;
+  const resource = (() => {
+    if (resourceProp) {
+      if (Array.isArray(resourceProp)) {
+        return resourceProp.join(', ');
+      }
+
+      return String(resourceProp);
+    }
+
+    return '';
+  })();
   const hasResourceAndType = resource && type;
   const isSingleResource = resource.split(', ').length === 1;
   const title = (() => {
-    // TODO: missing title
+    if (titleProp) {
+      return titleProp;
+    }
 
     if (hasResourceAndType) {
       if (isSingleResource) {
@@ -42,7 +68,9 @@ export default function DeleteConfirmModal({
     return t('DELETE');
   })();
   const tip = (() => {
-    // TODO: missing description
+    if (tipProp) {
+      return tipProp;
+    }
 
     if (deleteCluster) {
       return t('UNBIND_CLUSTER_DESC', { name: resource });
@@ -70,16 +98,35 @@ export default function DeleteConfirmModal({
   })();
 
   return (
-    <Modal visible={true} width={504} header={null} footer={null} closable={false}>
-      <StyledForm>
-        <Header>
-          <StyledCloseIcon variant="light" size={16} />
-          <Title as="h5">{title}</Title>
-        </Header>
-        <FormItem name="confirm" label={<p dangerouslySetInnerHTML={{ __html: tip }} />}>
-          <Input name="confirm" placeholder={resource} autoFocus={true} />
-        </FormItem>
-      </StyledForm>
+    <Modal visible={true} width={504} header={null} footer={null} closable={false} {...rest}>
+      <Form>
+        <Body>
+          <Header>
+            <CloseIconWrapper>
+              <Close variant="light" size={16} />
+            </CloseIconWrapper>
+            <Title as="h5">{title}</Title>
+          </Header>
+          <Content>
+            <Tip as="p" dangerouslySetInnerHTML={{ __html: tip }} />
+            <FormItem name="confirm">
+              <Input name="confirm" placeholder={resource} autoFocus={true} />
+            </FormItem>
+          </Content>
+        </Body>
+        <Footer>
+          <StyledButton>{t('CANCEL')}</StyledButton>
+          <StyledButton
+            type="submit"
+            color="error"
+            shadow
+            loading={confirmLoading}
+            disabled={confirmLoading || (resource ? true : false)}
+          >
+            {t('OK')}
+          </StyledButton>
+        </Footer>
+      </Form>
     </Modal>
   );
 }
--- packages/shared/src/components/DeleteConfirmModal/styles.ts
+++ packages/shared/src/components/DeleteConfirmModal/styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import { Form, Text } from '@kubed/components';
-import { Close } from '@kubed/icons';
+import { Text, Button } from '@kubed/components';
 
-export const StyledForm = styled(Form)`
+export const Body = styled.div`
   padding: 12px;
 `;
 
@@ -12,14 +11,56 @@ export const Header = styled.div`
 `;
 
 export const Title = styled(Text)`
-  margin: 0 0 0 8px;
+  margin: 0;
   font-size: 14px;
   font-weight: 600;
   line-height: 20px;
   color: ${props => props.theme.palette.accents_7};
 `;
 
-export const StyledCloseIcon = styled(Close)`
+export const CloseIconWrapper = styled.div`
+  margin-right: 8px;
+  width: 16px;
+  height: 16px;
   border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgba(202, 38, 33, 0.2);
   background-color: ${props => props.theme.palette.colors.red[2]};
+`;
+
+export const Content = styled.div`
+  margin-top: 12px;
+  padding: 12px;
+  border-radius: 4px;
+  background-color: ${props => props.theme.palette.accents_0};
+
+  .form-item-wrapper {
+    margin-bottom: 0;
+
+    .form-item {
+      margin-top: 12px;
+    }
+  }
+`;
+
+export const Tip = styled(Text)`
+  margin: 0;
+  font-size: 12px;
+  line-height: 1.67;
+  color: ${props => props.theme.palette.accents_6};
+  word-break: normal;
+`;
+
+export const Footer = styled.div`
+  padding: 10px 20px;
+  border-radius: 0 0 4px 4px;
+  background-color: ${props => props.theme.palette.accents_1};
+  text-align: right;
+`;
+
+export const StyledButton = styled(Button)`
+  margin-left: 10px;
+
+  &:first-of-type {
+    margin-left: 0;
+  }
 `;
